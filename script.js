6f264b9bdbbc1f2d9dd5a00b5c18399c029b46f4
abduvali0626 <umarrrrrrrrrrrrrrr63@gmail.com>
2025-02-05 14:57:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let body = document.getElementById("by");
+let btn1 = document.getElementById("b1");
+let btn2 = document.getElementById("b2");
+
+console.log(btn1);
+
+function lightMode() {
+    body.style.color = "black";
+    body.style.backgroundColor = "white";
+}
+
+function darkMode() {
+  body.style.backgroundColor = "black";
+  body.style.color = "white";
+}
